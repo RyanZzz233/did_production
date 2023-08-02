65e4071e30938f10e2ed7c2feaf136107e8d93af
RyanZzz233 <zhouyr.ryan@gmail.com>
2023-08-02 11:59:57 +0800

    debug

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -15,9 +15,9 @@ export const GET = async (request) => {
 
   try {
     await connect();
-    console.log(`Searching for posts with domain: ${domain}`);
+    //console.log(`Searching for posts with domain: ${domain}`);
     const posts = await Post.find(query);
-    console.log(`Found posts: ${JSON.stringify(posts)}`);
+    //console.log(`Found posts: ${JSON.stringify(posts)}`);
     //await db.connection.close();
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
